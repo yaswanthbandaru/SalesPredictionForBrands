76f998bdbb78d3c441f0d6b61e89c964a010173a
yaswanthbandaru <yaswanthbandaru424@gamil.com>
2021-09-14 22:27:37 +0530

    Create SalesPredicition.py

---
+++ SalesPrediction.py
@@ -0,0 +1,10 @@
+
+
+#Load libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#collect the data
+data = pd.read_csv('SalesDataProductWise.xlsx-Sheet1.csv')
+data.head()
