f93b6d70e15c2a48534df889f4518c43271044c7
yaswanthbandaru <yaswanthbandaru424@gamil.com>
2021-09-16 10:02:07 +0530

    update salesprediction.py

--- SalesPrediction.py
+++ SalesPrediction.py
@@ -4,7 +4,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #collect the data
 data = pd.read_csv('SalesDataProductWise.xlsx-Sheet1.csv')
-data.head()
+print(data.head())
+print("-------------------------------------------------------")
+print("Data Discription:")
+print(data.describe())
+print("-------------------------------------------------------")
+print("data iformation:")
+print(data.info())
+print("-------------------------------------------------------")
+
+plt.plot(data.isnull())
